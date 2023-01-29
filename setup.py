03a0d6ea2eaef7b8c896602b007196ede686e1de
Citrovex <95438156+Citrovex@users.noreply.github.com>
2023-01-29 21:16:54 +0200

    init

---
+++ setup.py
@@ -0,0 +1,2 @@
+# Module used to build and distribute Python packages. 
+# It typically contains information about the package, such as its name, version, and dependencies, as well as instructions for building and installing the package.
